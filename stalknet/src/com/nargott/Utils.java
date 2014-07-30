a0e5361aaa09b7232ed7a5a805429770b8a16661
Nargott <KiberPUNK.mail@gmail.com>
2014-07-31 00:23:21 +0300

    Work on sendMail

--- stalknet/src/com/nargott/Utils.java
+++ stalknet/src/com/nargott/Utils.java
@@ -1,12 +1,18 @@
 package com.nargott;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Random;
 import java.util.regex.Pattern;
 
+import com.dumbster.smtp.SimpleSmtpServer;
+import com.dumbster.smtp.SmtpMessage;
+
+import org.apache.commons.mail.*;
+
 public class Utils {
 
-	static public String MD5(String src) {
+	public static final String MD5(String src) {
 		try {
 	        java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
 	        byte[] array = md.digest(src.getBytes());
@@ -40,4 +46,47 @@ public class Utils {
 	    }
 	    return new String(password);
 	}
+	
+	public void sendEmail(
+		    String aFromEmailAddr, String aToEmailAddr,
+		    String aSubject, String aBody
+		  ) {
+		Email email = new SimpleEmail();
+		email.setHostName("smtp.googlemail.com");
+		email.setSmtpPort(465);
+		email.setAuthenticator(new DefaultAuthenticator("username", "password"));
+		email.setSSLOnConnect(true);
+		email.setSubject(aSubject);
+		try {
+			email.setFrom(aFromEmailAddr);
+			email.setMsg(aBody);
+			email.addTo(aToEmailAddr);
+			email.send();
+		} catch (EmailException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void testSend() {
+	    SimpleSmtpServer server = SimpleSmtpServer.start();
+
+	    try {
+	      // Submits an email using javamail to the email server listening on port 25 
+	      // (method not shown here). Replace this with a call to your app logic.
+	      sendEmail("sender@here.com", "receiver@there.com", "Test", "Test Body" );
+	    } catch(Exception e) {
+	      e.printStackTrace();
+	      System.out.println("Unexpected exception: "+e);
+	    }
+
+	    server.stop();
+
+	    assertTrue(server.getReceivedEmailSize() == 1);
+	    Iterator emailIter = server.getReceivedEmail();
+	    SmtpMessage email = (SmtpMessage)emailIter.next();
+	    assertTrue(email.getHeaderValue("Subject").equals("Test"));
+	    assertTrue(email.getBody().equals("Test Body"));	
+	  }
 }
